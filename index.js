707a2f8699ed2c88e1cfa7b1ec9927c9b40d454b
yashodip deore <yashodipdeore2006@gmail.com>
2026-06-21 21:41:26 +0530

    starter files, dir and code is added

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+
+
+
+//===========================================
+const app = express();
+
+
+//===========================================
+
+
+
+
+
+//============================================
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log('server is running at : http://localhost:' + PORT);
+});
