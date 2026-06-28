52522d23e3ae0a5f931d538eb3bd06f6e4839dea
yashodip deore <yashodipdeore2006@gmail.com>
2026-06-28 21:27:32 +0530

    fix: changes logo to logoURL in cloudinary file upload and added process.env prefix to CLOUDINARY_API_SECRET

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import fileUpload from 'express-fileupload';
+import bodyParser from 'body-parser';
 
 
 //--------- Local modules -----------------
@@ -8,13 +10,22 @@ import userRoutes from './routes/user.routes.js';
 
 
 //===========================================
-dotenv.config();
+dotenv.config(); // .env File configuration
 
-const app = express();
-ConnectDB();
+const app = express(); // App
 
+ConnectDB(); //DB-Connection
+
+//Middleware to convert "json" object into js Objects.
 app.use(express.json());
 
+//Middleware to to store files temporary in temp dir when they are being uploaded on cloudinary.
+app.use(fileUpload({
+  useTempFiles: true,
+  tempFileDir: '/temp/'
+}));
+
+//User Router
 app.use('/api/v1/user', userRoutes);
 
 
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,16 +1,60 @@
 import express from "express";
 import { Router } from "express";
+import bcrypt from 'bcrypt';
+import mongoose from "mongoose";
 
+//------- Local modules -------
+import User from '../models/user.model.js';
+import cloudinary from '../config/cloudinary.js';
 
 //===========================================
+//------- Middlewares ------
 const router = Router();
 
 
 //===========================================
-router.post('/signup', (req, res) => {
-  res.send('Hii Buddy...');
-});
+//-------- Routes -----------
 
+router.post('/signup', async (req, res) => {
+  try {
+    //Password Encryption
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    console.log('Hashed password : ', hashedPassword);
+
+    const uploadImage = await cloudinary.uploader.upload(
+      req.files.logoURL.tempFilePath
+    );
+    console.log('IMAGE ', uploadImage);
+
+
+    //Creating new user entry
+    const newUser = new User(
+      {
+        _id: new mongoose.Types.ObjectId(),
+        channelName: req.body.channelName,
+        email: req.body.email,
+        phone: req.body.phone,
+        password: hashedPassword, //Encrypted password
+        logoUrl: uploadImage.secure_url,
+        logoId: uploadImage.public_id
+      }
+    );
+
+    //inserting new user entry to DB
+    let user = await newUser.save();
+
+    res.status(201).json({
+      user
+    });
+
+  } catch (err) {
+    console.error(err);
+    res.status(401).json({
+      //inserting new user entry to DB
+      message: err.message
+    });
+  };
+});
 
 
 
