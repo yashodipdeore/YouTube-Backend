7693d9b6883a1307a8e0ef27062e91118c345fd1
yashodip deore <yashodipdeore2006@gmail.com>
2026-07-23 08:46:47 +0530

    Created comment.model.js and implemented comments model

---
+++ models/commet.model.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+
+
+const commentsSchema = new mongoose.Schema({
+  _id: mongoose.Schema.Types.ObjectId,
+  video_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Video',
+    required: true
+  },
+  user_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  commentsText: {
+    type: String,
+    required: true,
+  },
+}, { timestamps: true });
+
+const commentModel = mongoose.model('Comment', commentsSchema);
+
+export default commentModel;
